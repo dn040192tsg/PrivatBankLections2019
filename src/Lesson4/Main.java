74428c97d0a05a34513ed66577c09950d599d09a
dn040192tsg <sturgelia@gmail.com>
2019-08-12 23:38:31 +0300

    Initial commit

---
+++ src/Lesson4/Main.java
@@ -0,0 +1,16 @@
+package Lesson4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArraysHW arr = new ArraysHW();
+
+        arr.stringArray();
+        arr.charArray();
+        arr.doubleArray();
+        arr.jaggedArray();
+        arr.twoDimensional();
+
+    }
+}
